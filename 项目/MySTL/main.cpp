730b5b33ae2a4e73996bc3e8d38851e401f92426
HeXinlang <204424646@qq.com>
2019-12-17 22:08:16 +0800

    nothing

--- 项目/MySTL/main.cpp
+++ 项目/MySTL/main.cpp
@@ -1,34 +1,7 @@
 #include <iostream>
-#include <vector>
-#include <memory>
-#include <string.h>
-#include "myhead.h"
-#include "Contant/stl_vector.h"
-#include "Contant/stl_list.h"
-#pragma pack(2)
-struct test1
-{
-    char a;
-    double b;
-    int c;
-    int d;
-    int e;
-};
-
-struct test2
-{
-    char s;
-    int i;
-    test1 t;
-};
-
-int&& test()
-{
-    int a = 1;
-    return std::move(a);
-}
+using namespace std;
 
 int main()
 {
-   std::cout << test() << std::endl;
+    cout << "hello world" << endl;
 }
